ba79d82207c883dd8db102902078c18833bf07d2
Alwaysproblem <reganyang0415@gmail.com>
2021-04-25 13:35:44 +0800

    update permission

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ wget https://raw.githubusercontent.com/Alwaysproblem/IPU-setup/main/executable/I
 wget https://raw.githubusercontent.com/Alwaysproblem/IPU-setup/main/executable/popvision_enable -O $HOME/.ipuenv/popvision_enable
 wget https://raw.githubusercontent.com/Alwaysproblem/IPU-setup/main/executable/popvision_enable_oom -O $HOME/.ipuenv/popvision_enable_oom
 wget https://raw.githubusercontent.com/Alwaysproblem/IPU-setup/main/executable/setupVscode -O $HOME/.ipuenv/setupVscode
+
+chmod -R +x "$HOME/.ipuenv" 
